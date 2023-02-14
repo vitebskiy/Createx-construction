f5e32188f49ca8445fb5a956669f532d144dac4b
Miwgan <115034339+vitebskiy@users.noreply.github.com>
2023-02-14 10:39:01 +0300

    Доделал адаптацию

--- app/js/main.js
+++ app/js/main.js
@@ -13,6 +13,41 @@ if (document.querySelector('.we-offer')) {
   });
 }
 "use strict";
+
+var burger = document.querySelector('.burger');
+var menu = document.querySelector('.header__nav');
+var overlay = document.querySelector('.overlay');
+
+var disScroll = function disScroll() {
+  var pagePosition = window.scrollY;
+  document.body.classList.add('dis-scroll');
+  document.body.dataset.position = pagePosition;
+  document.body.style.top = -pagePosition + 'px';
+};
+
+var enScroll = function enScroll() {
+  var pagePosition = parseInt(document.body.dataset.position, 10);
+  document.body.style.top = 'auto';
+  document.body.classList.remove('dis-scroll');
+  window.scrollTo({
+    top: pagePosition,
+    left: 0
+  });
+  document.body.removeAttribute('data-position');
+};
+
+burger.addEventListener('click', function (e) {
+  burger.classList.toggle('burger--active');
+  menu.classList.toggle('header__nav--active');
+  overlay.classList.toggle('overlay--active');
+
+  if (burger.classList.contains('burger--active')) {
+    disScroll();
+  } else {
+    enScroll();
+  }
+});
+"use strict";
 "use strict";
 
 var modal = new GraphModal();
